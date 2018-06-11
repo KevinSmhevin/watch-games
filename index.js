2d7da80e21f424f4cf31098aae9e2c1b755dca13
Kevin Paras <kevincparas@gmail.com>
2018-06-10 19:49:15 -0700

    added CSS and made responsive

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ const STATE = {
         }]
     }
 }
+let totalStreams;
+let randomNumber;
+
+ 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+let errorMessage = `   <div>
+Sorry, we can't find that game :(
+<div>
+  `
+
+
 
 function getGameStream(searchGame, callback) {
     const twitchQueryData = {
@@ -18,7 +32,8 @@ function getGameStream(searchGame, callback) {
         query: `${searchGame}`,
         stream_type: 'live',
         format: 'jsonp',
-        limit: 1
+        limit: 1,
+        offset: randomNumber,
     }
     $.getJSON(TWITCH_STREAM_URL, twitchQueryData, callback)
 }
@@ -33,45 +48,59 @@ function watchSubmit() {
     });
 }
 
+function watchChangeStream() {
+    $('.main-content').on('click', '.change-streamer', event => {
+        getGameStream(STATE.query, processSearchResults);
+    })
+}
+
 function processSearchResults(data) {
     STATE.searchResults = data;
+    totalStreams = STATE.searchResults._total;
+    randomNumber = getRandomInt(totalStreams);
     render(STATE);
 }
 
-function watchChangeChannel() {
-    //this will load the change channel button
-}
-
 function render(state) {
-    displayTwitchStream(state.searchResults)
+    displayTwitchStream(state.searchResults);
 }
 
 function displayTwitchStream(data) {
+    console.log(data.status)
+    if (STATE.searchResults._total === 0) {
+    $('.main-content').html(errorMessage);
+    } else {
     const results = data.streams.map((item, index) => renderResult(item)).join("");
     $('.main-content').html(results);
+    }
 }
+
+
 
 function renderResult(result) {
     return `
-    <div>
-        <a href="${result.channel.url}" target="_blank">${result.channel.name}</a><br>
+    <div class="stream-section">
+        <a href="${result.channel.url}" target="_blank" class="streamer-name">${result.channel.name}</a>
         <iframe
+            class="stream-video"
             src="http://player.twitch.tv/?channel=${result.channel.name}"
             height="300"
-            width="400"
+            width="400"M
             frameborder="2"
             scrolling="no"
             allowfullscreen="true"
             autoplay="true">
-        </iframe>
+        </iframe><br>
+        <button type="button" class="change-streamer">Change Streamer</button>
     </div>
     `
 }
 //<a href="${result.channel.url}" target="_blank"><img src="${result.preview.medium}"</a>
 
 function loadPage() {
-    render(STATE);
+    // render(STATE);
     watchSubmit();
+    watchChangeStream();
 }
 
 $(loadPage);
